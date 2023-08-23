ed690a83774aa2d6576678e483eb8ee765920dbe
eodzh <eodzhadl0128@naver.com>
2023-08-23 23:14:46 +0900

    23/08/23

--- src/main/java/utils/JSFunction.java
+++ src/main/java/utils/JSFunction.java
@@ -27,17 +27,17 @@ public class JSFunction {
 		catch (Exception e) {}
 	}
 	
-//	public static void alertBack(String msg, JspWriter out) {
-//		
-//		try {
-//			String script = ""
-//					+ "<script>"
-//					+ "    alert('" + msg + "');"
-//					+ "    history.back();"
-//					+ "</script>";
-//			out.println(script);
-//		}
-//		catch (Exception e) {}
-//	}
+	public static void alertBack(String msg, JspWriter out) {
+		
+		try {
+			String script = ""
+					+ "<script>"
+					+ "    alert('" + msg + "');"
+					+ "    history.back();"
+					+ "</script>";
+			out.println(script);
+		}
+		catch (Exception e) {}
+	}
 }
 
